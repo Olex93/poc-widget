37e1ec2b7e6265ba04cfd1fda6c3bed4179f5d7b
Alex Foster <a.foster@fourleaf.co.uk>
2021-07-09 17:14:38 +0100

    api call

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import "../scss/iframe.scss";
 import "../scss/typography.scss";
+import axios from "axios";
 
 export default function Iframe(props) {
   const { highlightColor, backgroundColor, companyName } = props;
+  const [token, setToken] = useState("");
+  const [jsMemory, setJsMemoryInfo] = useState({});
+
+  useEffect(() => {
+    setJsMemoryInfo(window.performance.memory);
+  }, []);
+
+  function login() {
+    console.log("hello");
+    axios
+      .post("https://reqres.in/api/login", {
+        email: "eve.holt@reqres.in",
+        password: "cityslicka",
+      })
+      .then((response) => {
+        setToken(response.data.token)
+        console.log(response)
+      });
+  }
+
   return (
     <Container fluid className="iframe p-3">
       <Row>
         <Col>
           <h4 style={{ color: highlightColor }}>Hey {companyName}</h4>
-          <p className="bodyText">I am a chatbox style widget</p>
+          {!token && (
+            <>
+              <p className="bodyText">I am an iframe widget</p>
+              <Button onClick={() => login()}>Log in using credentials</Button>
+            </>
+          )}
+          {token && (
+            <div>
+              <p>You successfully logged in with credentials.</p>
+              <p>This is your auth token returned from the api: {token}</p>
+            </div>
+          )}
+          <p>Some data about webpage javascript heap size only available in chrome:</p>
+          <ul>
+            {/* <li><strong>Js Heap Size Limit:</strong> {jsMemory.jsHeapSizeLimit}</li>
+            <li><strong>Total Js Heap Size:</strong> {jsMemory.}</li> */}
+          </ul>
         </Col>
       </Row>
     </Container>
